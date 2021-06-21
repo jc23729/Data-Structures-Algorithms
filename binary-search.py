cfff0100b80eeb7c11aa3f1ebf008ad53f9ffee0
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-06-20 21:57:39 -0400

    update

--- binary-search.py
+++ binary-search.py
@@ -25,8 +25,6 @@ while left <= right:
 return -1
         
 
-
-
 # [141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7]
 
 def findThreeLargestNumbers(array):
@@ -68,6 +66,34 @@ def shiftAndUpdate(array, num, idx):
 			#we want to update array of 1 to our number that we want to put
 			array [1] = num
 			#
+		else:
+			array [i] = array[i + 1]
+
+
+
+
+# [141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7]
+
+def findThreeLargestNumbers(array):
+    # Write your code here.
+    threeLargest = [None, None, None]
+	for num in array:
+		updateLargest(threeLargest, num)
+		return threeLargest
+
+	
+def updateLargest(threeLargest, num):
+	if threeLargest[2] is None or num > threeLargest[2]:
+		shiftAndUpdate(threeLargest, num, 2)
+	elif threeLargest[1] is None or num > threeLargest[1]:
+		shiftAndUpdate(threeLargest, num, 1) 
+	elif threeLargest(0) is None or num > threeLargest[0]:
+		shiftAndUpdate(threeLargest, num, 0)
+
+def shiftAndUpdate(array, num, idx):
+	for i in range (idx + 1):
+		if i == idx:
+			array [1] = num
 		else:
 			array [i] = array[i + 1]
 		
